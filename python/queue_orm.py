ff199c81b9524aeaa8cf3dc494f351fb0faace59
Dave Lahr <dlahr@broadinstitute.org>
2016-02-02 19:14:35 -0500

    python/queue_orm:  finish initial version of class to CRUD queue table in database

--- python/queue_orm.py
+++ python/queue_orm.py
@@ -1,3 +1,8 @@
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
 
 
 class QueueOrm(object):
@@ -11,8 +16,32 @@ class QueueOrm(object):
         self.queue_type_name = queue_type_name
 
     def delete(self, cursor):
-        pass
+        if self.id is not None:
+            logger.debug("deleting self.id:  {}".format(self.id))
+            cursor.execute("delete from queue where id = ?", (self.id,))
+        else:
+            raise Exception("QueueOrm delete cannot delete when self.id is None")
+
+    def create(self, cursor):
+        if self.plate_id is not None and self.queue_type_id is not None:
+            cursor.execute("insert into queue (plate_id, queue_type_id) values (?, ?)",
+                (self.plate_id, self.queue_type_id))
+
+            self.id = cursor.lastrowid
+        else:
+            raise Exception("QueueOrm create cannot create when one of these is None - self.plate_id:  {}  self.queue_type_id:  {}".format(self.plate_id, self.queue_type_id))
+
+    def __str__(self):
+        return " ".join(["{}:{}".format(k,v) for (k,v) in self.__dict__.items()])
 
 
 def get_by_plate_id_queue_type_id(cursor, plate_id, queue_type_id):
-    pass
+    cursor.execute("""select q.id, plate_id, datetime_added, q.queue_type_id, qt.name
+from queue q
+join queue_type qt on qt.id = q.queue_type_id
+where plate_id = ? and q.queue_type_id = ?""", (plate_id, queue_type_id))
+
+    r = [QueueOrm(id=x[0], plate_id=x[1], datetime_added=x[2], queue_type_id=x[3],
+        queue_type_name=x[4]) for x in cursor]
+
+    return r
--- python/test_queue_orm.py
+++ python/test_queue_orm.py
@@ -1,5 +1,18 @@
 import unittest
 import queue_orm as qo
+import sqlite3
+import sys
+import build_database
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+conn = build_database.build(":memory:")
+
+default_queue_type_id = 2
+default_queue_type_name = "fake queue type"
 
 
 class TestQueueOrm(unittest.TestCase):
@@ -8,12 +21,77 @@ class TestQueueOrm(unittest.TestCase):
         assert hasattr(my_qo, "id")
         assert hasattr(my_qo, "plate_id")
 
+    def test_create(self):
+        cursor = conn.cursor()
+
+        my_qo = qo.QueueOrm(plate_id=1, queue_type_id=default_queue_type_id)
+
+        my_qo.create(cursor)
+
+        cursor.execute("select id, plate_id, queue_type_id from queue where plate_id=?",
+            (my_qo.plate_id,))
+
+        r = [x for x in cursor][0]
+        logger.debug("r:  {}".format(r))
+
+        assert r[0] == my_qo.id
+        assert r[2] == my_qo.queue_type_id
+
+        conn.rollback()
+
     def test_delete(self):
-        my_qo = qo.QueueOrm()
-        my_qo.delete(None)
+        cursor = conn.cursor()
+
+        my_qo = qo.QueueOrm(plate_id=1, queue_type_id=2)
+        my_qo.create(cursor)
+        my_qo.delete(cursor)
+
+        cursor.execute("select count(*) from queue where id = ?", (my_qo.id,))
+        r = [x for (x,) in cursor][0]
+        assert r == 0, r
+
+        conn.rollback()
+        cursor.close()
+        cursor = conn.cursor()
+
+        with self.assertRaises(Exception) as context:
+            my_qo.id = None
+            my_qo.delete(cursor)
+        assert context.exception
+        logger.debug("context.exception:  {}".format(context.exception))
+        assert "cannot delete when self.id is None" in str(context.exception)
+
+        cursor.close()
 
     def test_get_by_plate_id_queue_type_id(self):
-        qo.get_by_plate_id_queue_type_id(None, None, None)
+        cursor = conn.cursor()
+
+        plate_id_db_id_map = {}
+        N = 3
+        for plate_id in range(11, 11+N):
+            my_qo = qo.QueueOrm(plate_id=plate_id, queue_type_id=default_queue_type_id)
+            my_qo.create(cursor)
+            plate_id_db_id_map[plate_id] = my_qo.id
+
+        for (plate_id, db_id) in plate_id_db_id_map.items():
+            r = qo.get_by_plate_id_queue_type_id(cursor, plate_id, default_queue_type_id)
+            assert len(r) == 1, len(r)
+            r = r[0]
+            logger.debug("r:  {}".format(r))
+            assert r.id == db_id, r.id
+
+        conn.rollback()
+        cursor.close()
+
 
 if __name__ == "__main__":
+    setup_logger.setup(verbose=True)
+
+    cursor = conn.cursor()
+    cursor.execute("insert into queue_type (id,name) values (?, ?)",
+        (default_queue_type_id, default_queue_type_name))
+    cursor.close()
+
     unittest.main()
+
+    conn.close()
