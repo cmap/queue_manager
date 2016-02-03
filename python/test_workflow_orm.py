9280912b705219cd6de66948cc20f4e60ad6e7b3
Dave Lahr <dlahr@broadinstitute.org>
2016-02-02 22:27:14 -0500

    python/workflow_orm:  add functionality to retrieve by plate id and previous queue_type_id

--- python/test_workflow_orm.py
+++ python/test_workflow_orm.py
@@ -1,25 +1,118 @@
-import unittest
 import workflow_orm as wo
 import logging
+import unittest
+import sqlite3
+import sys
+import build_database
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+conn = build_database.build(":memory:")
+
+default_queue_type_map = {2:"fake queue type", 3:"another fqt", 5:"third fqt"}
 
 
 class TestWorkflowOrm(unittest.TestCase):
     def test___init__(self):
-        my_wo = wo.WorkflowOrm()
+        my_wo = wo.WorkflowOrm(id=3)
         assert hasattr(my_wo, "id")
         assert hasattr(my_wo, "plate_id")
+        assert my_wo.id == 3, my_wo.id
 
     def test_delete(self):
-        my_wo = wo.WorkflowOrm()
-        my_wo.delete(None)
+        cursor = conn.cursor()
+        my_wo = wo.WorkflowOrm(plate_id=2, prev_queue_type_id=3,next_queue_type_id=5)
+        my_wo.create(cursor)
+
+        cursor.execute("select count(*) from workflow where id = ?", (my_wo.id,))
+        r = [x for (x,) in cursor][0]
+        assert r == 1, r
+
+        my_wo.plate_id = None
+        my_wo.prev_queue_type_id = None
+        my_wo.next_queue_type_id = None
+        my_wo.delete(cursor)
+        cursor.execute("select count(*) from workflow where id = ?", (my_wo.id,))
+        r = [x for (x,) in cursor][0]
+        assert r == 0, r
+
+        my_wo.id = None
+        with self.assertRaises(Exception) as context:
+            my_wo.delete(cursor)
+        assert context.exception
+        logger.debug("context.exception:  {}".format(context.exception))
+        assert "WorkflowOrm delete cannot delete when self.id is None" in str(context.exception)
+
+        conn.rollback()
+        cursor.close()
 
     def test_create(self):
-        my_wo = wo.WorkflowOrm()
-        my_wo.create(None)
+        cursor = conn.cursor()
+        my_wo = wo.WorkflowOrm(plate_id=2, prev_queue_type_id=3,next_queue_type_id=5)
+        my_wo.create(cursor)
+
+        assert my_wo.id is not None
+
+        cursor.execute("select count(*) from workflow where plate_id=2 and prev_queue_type_id=3 and next_queue_type_id=5")
+        r = [x for (x,) in cursor][0]
+        assert r == 1, r
+
+        my_wo.id = None
+        my_wo.plate_id = None
+        with self.assertRaises(Exception) as context:
+            my_wo.create(cursor)
+        assert context.exception
+        logger.debug("context.exception:  {}".format(context.exception))
+        assert "WorkflowOrm create cannot create when one of these is None" in str(context.exception)
+
+        conn.rollback()
+        cursor.close()
 
     def test_get_by_plate_id_prev_queue_type_id(self):
-        r = wo.get_by_plate_id_prev_queue_type_id(None, None, None)
+        cursor = conn.cursor()
+
+        plate_id_db_id_map = {}
+        N = 3
+        for plate_id in range(11, 11+N):
+            my_wo = wo.WorkflowOrm(plate_id=plate_id, prev_queue_type_id=2,
+                next_queue_type_id=3)
+            my_wo.create(cursor)
+            plate_id_db_id_map[plate_id] = my_wo.id
+
+        cursor.execute("""select * from workflow w
+            join queue_type qtp on qtp.id = w.prev_queue_type_id
+            join queue_type qtn on qtn.id = w.next_queue_type_id
+            """)
+        r = [x for x in cursor]
+        logger.debug("r:  {}".format(r))
+
+        for (plate_id, db_id) in plate_id_db_id_map.items():
+            r = wo.get_by_plate_id_prev_queue_type_id(cursor, plate_id, 2)
+            assert len(r) == 1, len(r)
+            r = r[0]
+            logger.debug("r:  {}".format(r))
+            assert r.id == db_id, r.id
+            assert r.next_queue_type_id == 3, r.next_queue_type_id
+            assert r.prev_queue_type_name == "fake queue type", r.prev_queue_type_name
+            assert r.next_queue_type_name == "another fqt", r.next_queue_type_name
+
+        conn.rollback()
+        cursor.close()
 
 
 if __name__ == "__main__":
+    setup_logger.setup(verbose=True)
+
+    cursor = conn.cursor()
+    for (my_id,name) in default_queue_type_map.items():
+        cursor.execute("insert into queue_type (id,name) values (?, ?)",
+            (my_id, name))
+    conn.commit()
+    cursor.close()
+
     unittest.main()
+
+    conn.close()
--- python/workflow_orm.py
+++ python/workflow_orm.py
@@ -1,3 +1,9 @@
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
 
 class WorkflowOrm(object):
     def __init__(self, id=None, plate_id=None, prev_queue_type_id=None,
@@ -11,10 +17,37 @@ class WorkflowOrm(object):
         self.next_queue_type_name = next_queue_type_name
 
     def delete(self, cursor):
-        pass
+        if self.id is not None:
+            logger.debug("deleting self.id:  {}".format(self.id))
+            cursor.execute("delete from workflow where id = ?", (self.id,))
+        else:
+            raise Exception("WorkflowOrm delete cannot delete when self.id is None")
+
 
     def create(self, cursor):
-        pass
+        if self.plate_id is not None and self.prev_queue_type_id is not None and self.next_queue_type_id is not None:
+            cursor.execute("""insert into workflow (plate_id, prev_queue_type_id, next_queue_type_id) values (?, ?, ?)""",
+                (self.plate_id, self.prev_queue_type_id, self.next_queue_type_id))
+
+            self.id = cursor.lastrowid
+        else:
+            raise Exception("WorkflowOrm create cannot create when one of these is None - self.plate_id:  {}  self.prev_queue_type_id:  {}  self.next_queue_type_id:  {}".format(self.plate_id, self.prev_queue_type_id, self.next_queue_type_id))
+
+    def __str__(self):
+        return " ".join(["{}:{}".format(k,v) for (k,v) in self.__dict__.items()])
+
 
 def get_by_plate_id_prev_queue_type_id(cursor, plate_id, prev_queue_type_id):
-    pass
+    logger.debug("plate_id:  {}  prev_queue_type_id:  {}".format(plate_id, prev_queue_type_id))
+
+    cursor.execute("""select w.id, plate_id, prev_queue_type_id, qtp.name, next_queue_type_id, qtn.name
+ from workflow w
+ join queue_type qtp on qtp.id = w.prev_queue_type_id
+ join queue_type qtn on qtn.id = w.next_queue_type_id
+ where plate_id = ? and prev_queue_type_id = ?""", (plate_id, prev_queue_type_id))
+
+    r = [WorkflowOrm(id=x[0], plate_id=x[1], prev_queue_type_id=x[2],
+        prev_queue_type_name=x[3], next_queue_type_id=x[4], next_queue_type_name=x[5])
+        for x in cursor]
+
+    return r
