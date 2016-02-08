11221008566d6bb0598bc45a7d3c2e436b76aba7
Dave Lahr <dlahr@broadinstitute.org>
2016-02-08 12:24:11 -0500

    python/workflow_template_orm: fill in methods to retrieve workflow templates

--- python/test_workflow_template_orm.py
+++ python/test_workflow_template_orm.py
@@ -1,5 +1,13 @@
+import logging
+import setup_logger
 import unittest
 import workflow_template_orm as wto
+import build_database
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+conn = None
 
 
 class TestWorkflowTemplateOrm(unittest.TestCase):
@@ -11,10 +19,73 @@ class TestWorkflowTemplateOrm(unittest.TestCase):
         assert my_wto.queue_type_pairs is not None
         assert len(my_wto.queue_type_pairs) == 0, len(my_wto.queue_type_pairs)
 
+    def test__build_from_cursor_query(self):
+        cursor = [(1, "fake workflow template", 2, "fake prev queue type item", 3, "fake next queue type item"),
+            (1, "this should be ignored", 5, "another prev queue type item", 7, "my next queue type item"),
+            (11, "another workflow template", 13, "third prev queue type item", 17, "third next queue type item"),
+            (11, "another thing to ignore", 19, "fourth prev qti", 23, "fourth next qti")]
+
+        r = wto._build_from_cursor_query(cursor)
+        assert r
+        assert len(r) == 2, len(r)
+        for (i, my_wto) in enumerate(r):
+            logger.debug("my_wto:  {}".format(my_wto))
+            assert len(my_wto.queue_type_pairs) == 2, len(my_wto.queue_type_pairs)
+
+            expected_index = i*2
+            assert my_wto.id == cursor[expected_index][0], my_wto.id
+            assert my_wto.name == cursor[expected_index][1], my_wto.name
+
+            for qt_index in range(2):
+                qt_expected_index = expected_index + qt_index
+
+                pqt = my_wto.queue_type_pairs[qt_index][0]
+                assert pqt.id == cursor[qt_expected_index][2], pqt.id
+                assert pqt.name == cursor[qt_expected_index][3], pqt.name
+
+                nqt = my_wto.queue_type_pairs[qt_index][1]
+                assert nqt.id == cursor[qt_expected_index][4], nqt.id
+                assert nqt.name == cursor[qt_expected_index][5], nqt.name
 
     def test_get_by_id(self):
-        r = wto.get_by_id(None, None)
+        cursor = conn.cursor()
+        cursor.execute("select id from workflow_template limit 0,1")
+        test_id = [x for (x,) in cursor][0]
+        logger.debug("test_id:  {}".format(test_id))
 
+        r = wto.get_by_id(cursor, test_id)
+        assert r is not None
+        logger.debug("r:  {}".format(r))
+        assert len(r) == 1, len(r)
+        r = r[0]
+        assert r.id == test_id, r.id
+        assert r.name is not None
+        assert len(r.queue_type_pairs) > 0
+        qtp = r.queue_type_pairs[0]
+        assert len(qtp) == 2, len(qtp)
+        assert qtp[0].id is not None
+        assert qtp[0].name is not None
+        assert qtp[1].id is not None
+        assert qtp[1].name is not None
+
+    def test_get_all(self):
+        cursor = conn.cursor()
+        cursor.execute("select count(*) from workflow_template")
+        expected_count = [x for (x,) in cursor][0]
+        logger.debug("expected_count:  {}".format(expected_count))
+
+        r = wto.get_all(cursor)
+        assert r is not None
+        logger.debug("r:  {}".format(r))
+        assert len(r) == expected_count, len(r)
 
 if __name__ == "__main__":
+    setup_logger.setup(verbose=True)
+
+    conn = build_database.build(":memory:")
+    build_database.insert_initial_prism_values(conn)
+    conn.commit()
+
     unittest.main()
+
+    conn.close()
--- python/workflow_template_orm.py
+++ python/workflow_template_orm.py
@@ -1,10 +1,60 @@
+import queue_type_orm
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+base_query = """select wt.id, wt.name, wtp.prev_queue_type_id, pqt.name, wtp.next_queue_type_id, nqt.name
+ from workflow_template wt
+ join workflow_template_pair wtp on wtp.workflow_template_id = wt.id
+ join queue_type pqt on pqt.id = wtp.prev_queue_type_id
+ join queue_type nqt on nqt.id = wtp.next_queue_type_id
+ """
+
 
 class WorkflowTemplateOrm(object):
-    def __init__(self, id=None, name=None, queue_type_pairs=[]):
+    def __init__(self, id=None, name=None):
         self.id = id
         self.name = name
-        self.queue_type_pairs = queue_type_pairs
+        self.queue_type_pairs = []
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        qtp = []
+        for x in self.queue_type_pairs:
+            qtp.append(str(x[0]) + "-->" + str(x[1]))
+        qtp_str = " ".join(qtp)
+        return "id:{} name:{} queue_type_pairs:{}".format(self.id, self.name, qtp)
 
 
 def get_by_id(cursor, workflow_template_id):
-    pass
+    query = base_query + " where wt.id = ?"
+    cursor.execute(query, (workflow_template_id,))
+    return _build_from_cursor_query(cursor)
+
+
+def get_all(cursor):
+    cursor.execute(base_query)
+    return _build_from_cursor_query(cursor)
+
+
+def _build_from_cursor_query(cursor):
+    r = {}
+    for (wt_id, name, prev_qt_id, prev_qt_name, next_qt_id, next_qt_name) in cursor:
+        logger.debug("wt_id:  {}".format(wt_id))
+        if wt_id not in r:
+            new_wto = WorkflowTemplateOrm(id=wt_id, name=name)
+            logger.debug("new_wto:  {}".format(new_wto))
+            r[wt_id] = new_wto
+
+        wto = r[wt_id]
+        logger.debug("wto:  {}".format(wto))
+
+        pqt = queue_type_orm.QueueTypeOrm(id=prev_qt_id, name=prev_qt_name)
+        npt = queue_type_orm.QueueTypeOrm(id=next_qt_id, name=next_qt_name)
+        wto.queue_type_pairs.append((pqt, npt))
+
+    return r.values()
