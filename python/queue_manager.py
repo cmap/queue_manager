69fbe9f34d4a065e64bf07b0cbb4bd95d016bad6
Dave Lahr <dlahr@broadinstitute.org>
2016-02-22 02:54:12 -0500

    python/queue_manager: add initial functionality for queue management - method for reporting a completion causes queues to be updated based on workflows

---
+++ python/queue_manager.py
@@ -0,0 +1,47 @@
+import workflow_orm
+import queue_orm
+import ConfigParser
+import sqlite3
+import os.path
+import logging
+import setup_logger
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+default_queue_manager_config_path = os.path.expanduser("~/.queue_manager.cfg")
+
+
+def report_completion(plate_id, completed_queue_type_id,
+    queue_manager_config_path=default_queue_manager_config_path):
+
+    logger.debug("queue_manager_config_path:  {}".format(queue_manager_config_path))
+
+    conn = _open_database_connection(queue_manager_config_path)
+    cursor = conn.cursor()
+
+    previous_queue_orm = queue_orm.get_by_plate_id_queue_type_id(cursor, plate_id,
+        completed_queue_type_id)
+    previous_queue_orm.delete(cursor)
+
+    workflows = workflow_orm.get_by_plate_id_prev_queue_type_id(cursor, plate_id,
+        completed_queue_type_id)
+
+    for w in workflows:
+        qo = queue_orm.QueueOrm(plate_id=plate_id, queue_type_id=w.next_queue_type_id)
+        qo.create(cursor)
+        w.delete(cursor)
+
+    conn.commit()
+    conn.close()
+
+
+def _open_database_connection(queue_manager_config_path):
+    logger.debug("queue_manager_config_path:  {}".format(queue_manager_config_path))
+
+    cp = ConfigParser.RawConfigParser()
+    cp.read(queue_manager_config_path)
+
+    conn = sqlite3.connect(cp.get("Database", "sqlite3_file_path"))
+
+    return conn
---
+++ python/test_queue_manager.py
@@ -0,0 +1,74 @@
+import unittest
+import logging
+import setup_logger
+import build_database
+import ConfigParser
+import os
+import queue_manager
+
+
+logger = logging.getLogger(setup_logger.LOGGER_NAME)
+
+cfg_path = "functional_tests/test_queue_manager/queue_manager.cfg"
+
+
+class TestQueueManager(unittest.TestCase):
+    def test__open_database_connection(self):
+        test_conn = queue_manager._open_database_connection(cfg_path)
+        assert test_conn is not None
+        cursor = test_conn.cursor();
+
+        cursor.execute("select count(*) from workflow_template_pair")
+        r = [x for x in cursor][0]
+        assert r > 0
+
+        cursor.execute("select count(*) from queue_type")
+        r = [x for x in cursor][0]
+        assert r > 0
+
+        cursor.execute("select count(*) from workflow_template")
+        r = [x for x in cursor][0]
+        assert r > 0
+
+        test_conn.close()
+
+    def test_report_completion(self):
+        conn = queue_manager._open_database_connection(cfg_path)
+        cursor = conn.cursor()
+
+        cursor.execute("insert into queue (plate_id, queue_type_id) values ('1', 1)")
+        cursor.execute("insert into workflow (plate_id, prev_queue_type_id, next_queue_type_id) values ('1', 1, 2)")
+
+        conn.commit()
+        conn.close()
+
+        queue_manager.report_completion("1", 1, queue_manager_config_path=cfg_path)
+
+        conn = queue_manager._open_database_connection(cfg_path)
+        cursor = conn.cursor()
+
+        cursor.execute("select queue_type_id from queue where plate_id='1'")
+        r = [x for (x,) in cursor]
+        assert len(r) == 1, len(r)
+        assert r[0] == 2, r[0]
+
+        cursor.execute("select count(*) from workflow where plate_id='1'")
+        r = [x for (x,) in cursor][0]
+        assert r == 0, r
+
+
+if __name__ == "__main__":
+    setup_logger.setup(verbose=True)
+
+    cp = ConfigParser.RawConfigParser()
+    cp.read(cfg_path)
+    db_path = cp.get("Database", "sqlite3_file_path")
+
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+    conn = build_database.build(db_path)
+    build_database.insert_initial_espresso_prism_values(conn)
+    conn.commit()
+
+    unittest.main()
