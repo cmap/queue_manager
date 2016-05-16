2ee0943884366482dd286499c4677aa73e862184
Dave Lahr <dlahr@broadinstitute.org>
2016-05-16 14:37:43 -0400

    database/prism_espresso_update002_insert_initial_values:  fix bug add missing step (yank --> roast) for LITMUS workflow

--- broadinstitute/queue_manager/database/prism_espresso_update002_insert_initial_values.sql
+++ broadinstitute/queue_manager/database/prism_espresso_update002_insert_initial_values.sql
@@ -36,6 +36,10 @@ insert into workflow_template_pair (workflow_template_id, prev_queue_type_id, ne
 insert into workflow_template_pair (workflow_template_id, prev_queue_type_id, next_queue_type_id)
     select wf.id, qt1.id, qt2.id
     from workflow_template wf, queue_type qt1, queue_type qt2
+    where wf.name="L1000 LITMUS" and qt1.name="yank" and qt2.name="roast";
+insert into workflow_template_pair (workflow_template_id, prev_queue_type_id, next_queue_type_id)
+    select wf.id, qt1.id, qt2.id
+    from workflow_template wf, queue_type qt1, queue_type qt2
     where wf.name="L1000 LITMUS" and qt1.name="roast" and qt2.name="litmus analysis";
 
 insert into workflow_template_pair (workflow_template_id, prev_queue_type_id, next_queue_type_id)
