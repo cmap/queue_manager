192ae2edc8b8145f48f10ca70940e15a3f9fbec2
Dave Lahr <dlahr@broadinstitute.org>
2017-03-31 17:30:49 -0400

    broadinstitute/queue_manager/database/prism_espresso_update004_insert_dpeaks_metricsify.sql fix name of dpeak_metricsify queue

--- broadinstitute/queue_manager/database/prism_espresso_update004_insert_dpeaks_metricsify.sql
+++ broadinstitute/queue_manager/database/prism_espresso_update004_insert_dpeaks_metricsify.sql
@@ -1,6 +1,6 @@
-insert into queue_type (name) values ("dpeaks_metricsify");
+insert into queue_type (name) values ("dpeak_metricsify");
 
 insert into workflow_template_pair (workflow_template_id, prev_queue_type_id, next_queue_type_id)
     select wf.id, qt1.id, qt2.id
     from workflow_template wf, queue_type qt1, queue_type qt2
-    where wf.name="L1000 espresso" and qt1.name="brew" and qt2.name="dpeaks_metricsify";
+    where wf.name="L1000 espresso" and qt1.name="brew" and qt2.name="dpeak_metricsify";
